195de5d46ddbcc6efd1f487cb77a26ba8ef6c7a4
Michael Arakilian <arakilian0@gmail.com>
2019-12-09 07:11:23 -0800

    MINOR update to config class and gitignore

--- src/pr.h
+++ src/pr.h
@@ -1,17 +1,21 @@
 // processor
 #include <iostream>
 #include <string>
-
 using namespace std;
+
+string ext5 = "a";
+string ext4 = "l";
+string ext3 = "o";
+string ext2 = "c";
+string ext1 = ".";
 
 class Processor
 {
     public:
 
     void process ( int argc, char** argv ) {
-      cout << "Usage: cola styles.cola <options> or cola <command> <options>" << endl;
-      cout << "Usage: cola styles.cola <options> or cola <command> <options>" << endl;
-      cout << "Usage: cola styles.cola <options> or cola <command> <options>" << endl;
-      cout << "Usage: cola styles.cola <options> or cola <command> <options>" << endl;
+      cout << "checking for valid file..." << endl;
+      // check last 5 characters of the 2nd char** entered
+      // checking: . c o l a
     }
 };
